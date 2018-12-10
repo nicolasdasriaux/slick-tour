99c8ada78763b0d3a53cbd8f2fa420401e074aeb
Nicolas Dasriaux <ndasriaux@gmail.com>
2018-12-10 17:47:32 +0100

    Fix table name

--- src/main/scala/slicktour/ecommerce/db/Items.scala
+++ src/main/scala/slicktour/ecommerce/db/Items.scala
@@ -2,7 +2,7 @@ package slicktour.ecommerce.db
 
 import ExtendedPostgresProfile.api._
 
-class Items(tag: Tag) extends Table[Item](tag, "itemss") {
+class Items(tag: Tag) extends Table[Item](tag, "items") {
   def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
   def name = column[String]("name")
   override def * = (id.?, name) <> ((Item.apply _).tupled, Item.unapply)
